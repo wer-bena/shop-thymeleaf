15e42730e154d57718a35c15f4dbdb921b4bdef6
Bernadeta G <grudzien.bernadeta@example.com>
2021-04-10 09:52:36 +0200

    Init project

---
+++ src/main/java/pl/edu/wszib/shopthymeleaf/ShopThymeleafApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.shopthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopThymeleafApplication.class, args);
+    }
+
+}
